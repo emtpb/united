642e07e84b6f8ff885f36395386e6e8178e301b1
Kevin Koch <kevink2@mail.uni-paderborn.de>
2020-04-29 19:45:02 +0200

    Add tests for initializing, multiplying, dividing and repr.

--- tests/test_united.py
+++ tests/test_united.py
@@ -1,4 +1,57 @@
-"""Tests for `united` package."""
+"""Tests for `sipyunits` package."""
 import pytest
+from collections import Counter
 
-from united import united
+from united.united import Unit
+
+
+def test_initializing():
+    a = Unit(["s"])
+    assert a.numerators == ["s"]
+    assert a.denominators == []
+    b = Unit(["kg"], ["s"])
+    assert b.numerators == ["kg"]
+    assert b.denominators == ["s"]
+    c = Unit(["C"])
+    assert Counter(c.numerators) == Counter(["A", "s"])
+    assert c.denominators == []
+    d = Unit(["V"])
+    assert Counter(d.numerators) == Counter(["m", "kg", "m"])
+    assert Counter(d.denominators) == Counter(["s", "s", "s", "A"])
+
+
+def test_multiplying():
+    a = Unit(["s"])
+    b = a * a
+    assert Counter(b.numerators) == Counter(["s", "s"])
+    assert a.numerators == ["s"]
+    c = Unit([], ["s"])
+    d = a * c
+    assert d.numerators == []
+    assert d.denominators == []
+    e = Unit(["V"])
+    f = Unit(["F"])
+    g = e * f
+    assert Counter(g.numerators) == Counter(["s", "A"])
+    assert g.denominators == []
+
+
+def test_dividing():
+    a = Unit(["s"])
+    b = a / a
+    assert b.numerators == []
+    assert b.denominators == []
+    c = Unit(["V"])
+    d = c / a
+    assert Counter(d.numerators) == Counter(["m", "m", "kg"])
+    assert Counter(d.denominators) == Counter(["s", "s", "s", "s", "A"])
+
+
+@pytest.mark.parametrize("numerator, denominator, expected",
+                         [(["s"], [], "s"), (["V", "A"], [], "W"), ([], ["V", "A"], "1/(W)"), (["V"], ["A"], "O"),
+                          (["m", "m", "kg"], ["s", "s", "s", "A"], "V"),
+                          ([], ["O"], "S"), ([], ["A", "s"], "1/(C)"), (["F"], ["C"], "1/(V)"),
+                          (["V", "s"], [], "(m*m*kg)/(s*C)")])
+def test_repr(numerator, denominator, expected):
+    a = Unit(numerator, denominator)
+    assert repr(a) == expected
